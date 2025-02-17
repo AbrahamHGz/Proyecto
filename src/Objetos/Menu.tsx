08faf1010d96be586c1ed503a6ab180ed5297715
AbrahamHGz <158483775+AbrahamHGz@users.noreply.github.com>
2025-02-17 10:37:18 -0600

    Primeras ventanas del proyecto de react

---
+++ src/Objetos/Menu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu: React.FC = () => {
+    return (
+        <>
+            <div className="bg-slate-700">
+                <h1 className="text-lg">Hola soy home</h1>
+
+            </div>
+        </>        
+    )
+}
+
+export default Menu;
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+    return(
+        <>
+            <div className="bg-blue-300">
+                <h1 className="text-lg">Hola soy home</h1>
+
+            </div>
+        </>
+
+        
+    )
+}
+
+export default Home
