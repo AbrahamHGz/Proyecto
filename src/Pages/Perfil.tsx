b596b818c4d2a2828173d9062512592641f4a25c
AbrahamHGz <158483775+AbrahamHGz@users.noreply.github.com>
2025-02-22 14:06:43 -0600

    Cambios del perfil
    Cosas menores para el perfil

--- src/Pages/Perfil.tsx
+++ src/Pages/Perfil.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Publicaciones from "../Objetos/Publicaciones";
 import Menu from "../Objetos/Menu";
 const Perfil: React.FC = () => {
@@ -6,46 +7,40 @@ const Perfil: React.FC = () => {
         <>
             <Menu></Menu>
             
-           <div className="flex mx-40 grid grid-cols-2 items-center pt-26">
+           <div className="flex mx-40 grid grid-cols-2 items-center pt-26 mb-3">
                 <div className="flex">
+
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTELPl2WQuMBShrQaqe0IWYjLf_y2XRkhGNWcdLfADOPJ6KAJe84GaYOQ51__wkkbGfR78&usqp=CAU"
-                    alt="" className="size-20 "/>
+                    alt="" className="size-30 "/>
 
-                    <div>
-                        <p><strong className="text-2xl">"Usuario"</strong> <br />
-                            Descripcion <br /> 
-                            Contacto: Facebook
-                        </p>
+                    <div className="text-white px-3">
+
                     </div>  
-
-                    <div className="grid grid-cols-1 space-y-2 mx-5">
-                        <button className="p-1 bg-lime-200 rounded border-lime-600 
-                        hover:bg-lime-100 hover:border-lime-400 font-semibold"
-                        >Editar Perfil</button>
-                        <button className="p-1 bg-red-200  rounded border-red-600 
-                        hover:bg-red-100 hover:border-red-400 font-semibold">Cerrar Sesión</button>
-                    </div>
-
                    
                 </div>
 
-                <div className=" mt-2 flex justify-end">
-                    <div className="space-y-2">
-                        <p className="bg-sky-100 p-2 w-32 ">Seguidores: 20</p>
-                        <p className="bg-sky-100 p-2 w-32">Siguiendo: 1</p>    
-                    </div>
+
+                <div className=" mt-2 flex justify-end space-x-3">
+                    <Link to="" className="border p-2 hover:bg-slate-700 border-white text-white">Editar Perfil</Link>
+                    <Link to="/Login" className="border p-2 hover:bg-red-800 border-red-300 text-white px-8">Salir</Link>
 
                 </div>         
            </div>
 
            <div className="mx-40 space-x-2">
-                <button className="bg-sky-200 font-bold px-10 p-1 rounded hover:bg-gray-600">Publicaciones</button>
-                <button className="bg-sky-200 font-bold px-10 p-1 rounded hover:bg-gray-600">Favoritos</button>
+               <button className="bg-gray-400 font-bold px-10 p-1 rounded-t text-white hover:bg-gray-400">Acerca de mí</button>
+
+               <button className="bg-gray-500 font-bold px-10 p-1 rounded-t text-white hover:bg-gray-400">Publicaciones</button>
+               <button className="bg-gray-500 font-bold px-10 p-1 rounded-t text-white hover:bg-gray-400">Favoritos</button>
                 
            </div>
 
-           <div className="mx-40 bg-sky-200 p-4">
+           <div className="mx-40 bg-gray-400 p-4 rounded-b rounded-e">
                 <Publicaciones></Publicaciones>
+           </div>
+
+           <div className="p-2">
+
            </div>
 
         </>
