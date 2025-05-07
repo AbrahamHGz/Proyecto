9d94b73976c4ed88d914bbd8e9a008d59388a4f6
LeafMad <leafmadmedia23@gmail.com>
2025-05-06 23:52:46 -0600

    Pequeña Corrección Para Ingresar Sesion

--- BackEnd/routes/publicacion.js
+++ BackEnd/routes/publicacion.js
@@ -1,9 +1,11 @@
 import express from "express";
-import publicacionController from "../controllers/publicacion.js";
-const route = express.Router();
 import cors from "cors";
 import { verificarToken } from "../helpers/authentication.js";
+import publicacionController from "../controllers/publicacion.js";
 
+const route = express.Router();
+
+// Middleware CORS
 route.use(
   cors({
     origin: "http://localhost:5173",
@@ -12,14 +14,14 @@ route.use(
   })
 );
 
-route.post("/", verificarToken, publicacionController.create);
-route.put("/:id", verificarToken, publicacionController.update);
-route.put("/est/:id", verificarToken, publicacionController.updateEstatus);
-route.delete("/:id", verificarToken, publicacionController.delete);
-route.get("/withLikesCount", publicacionController.getAllWithLikesCount);
-route.get("/:id", publicacionController.getOne);
-route.get("/", publicacionController.getAll);
-route.get("/pub/:id", publicacionController.getAllIdUsu);
-route.get("/Nombre/:PUBnombre", publicacionController.getOneByNombre);
+// CRUD sobre publicaciones
+route.post('/',verificarToken ,publicacionController.create);
+route.get('/:id', publicacionController.getOne);
+route.get('/', publicacionController.getAll);
+route.get('/pub/:id',publicacionController.getAllIdUsu);
+route.get('/Nombre/:PUBnombre', publicacionController.getOneByNombre);
+route.put('/:id',verificarToken ,publicacionController.update);
+route.put('/est/:id',verificarToken ,publicacionController.updateEstatus);
+route.delete('/:id' ,verificarToken ,publicacionController.delete);
 
 export default route;
